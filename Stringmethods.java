92f3d14cc1d59474970679aa71913d94e20c7f05
Brandon <145504804+bunny2027@users.noreply.github.com>
2026-01-24 03:56:36 +0000

    10/31

--- Stringmethods.java
+++ Stringmethods.java
@@ -1,5 +1,20 @@
-public static void Stringmethods{
+public class Stringmethods{
+
+    public static void strings(){
     String my_name = "Brandon";
-    System.out.println = my_name;
-    
+    System.out.println(my_name);
+    String clipped = my_name.replace('B', 'A');
+    String clipped2 = clipped.substring(0, clipped.length() - 1) + "Z";
+    System.out.println(clipped2);
+
+    String url = "www.gatech.edu";
+    System.out.println(url);
+
+    String n_url = url.substring(4,url.length()- 3) +"1331";
+    String nn_url = n_url.replace(".","");
+    System.out.println(nn_url);
+    }
+    public static void main(String[] args){
+        strings();
+    }
 }
